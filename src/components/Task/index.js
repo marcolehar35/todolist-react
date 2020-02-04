6ccf53fa498e69657521cf5fd6588b16734f49da
marcolehar35 <lehartel.com@gmail.com>
2020-02-04 10:42:40 +0100

    Task avec des props

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,16 +1,32 @@
 // == Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 // == Import
 import './styles.css';
 
 // == Composant
-const Task = () => (
-  <li className="task">
-    <input type="checkbox" />
-    <span className="task-label">La tache</span>
-  </li>
-);
+const Task = ({ label, done }) => {
+  // traitements
+  const classCss = classNames('task', {
+    // la classe css 'task--done' ne sera présente que si done vaut true
+    'task--done': done,
+  });
+
+  return (
+    // dans le jsx
+    <li className={classCss}>
+      <input type="checkbox" checked={done} />
+      <span className="task-label">{label}</span>
+    </li>
+  );
+};
+
+Task.propTypes = {
+  label: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
+};
 
 // == Export
 export default Task;
