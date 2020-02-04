b013285ea6c879c4c4af2d19a9f4ac8def53428c
marcolehar35 <lehartel.com@gmail.com>
2020-02-04 11:28:02 +0100

    state et affichage des taches

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,18 +1,27 @@
 // == Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // == Import
 import './styles.css';
 import Task from 'src/components/Task';
 
 // == Composant
-const List = () => (
+const List = ({ tasks }) => (
   <ul id="tasks-list">
-    {/* done équivalent done={true} */}
-    <Task label="Dire bonjour" done />
-    <Task label="Une todo en React" done={false} />
+    {tasks.map((task) => (
+      <Task key={task.id} {...task} />
+    ))}
   </ul>
 );
+
+List.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+    }).isRequired,
+  ).isRequired,
+};
 
 // == Export
 export default List;
--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -1,20 +1,30 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
 
 // == Import
 import './style.css';
 import Form from 'src/components/Form';
 import Counter from 'src/components/Counter';
 import List from 'src/components/List';
+import tasksData from 'src/data/tasks';
+// selectors
+import { getTasksUndone } from 'src/selectors/tasks';
 
 // == Composant
-const Todolist = () => (
-  <div id="todo">
-    <Form />
-    <Counter count={123} />
-    <List />
-  </div>
-);
+const Todolist = () => {
+  // mise en place du state
+  const [tasks, setTasks] = useState(tasksData);
+  // utilisation
+  const count = getTasksUndone(tasks).length;
+
+  return (
+    <div id="todo">
+      <Form />
+      <Counter count={count} />
+      <List tasks={tasks} />
+    </div>
+  );
+};
 
 // == Export
 export default Todolist;
