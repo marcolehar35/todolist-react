e5761c8a3453c94e286793ffd1881f41a2ea1c3f
marcolehar35 <lehartel.com@gmail.com>
2020-02-04 09:13:29 +0100

    recup du style de la todo de départ

---
+++ src/components/Todolist/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './style.css';
+
+// == Composant
+const Todolist = () => (
+  <div id="todo">
+    todolist
+  </div>
+);
+
+// == Export
+export default Todolist;
