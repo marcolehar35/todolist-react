a5ace90e9d1e81d9a476aa1508080d29cbc33de6
Philippe Candille <philippe@oclock.io>
2020-02-17 10:21:38 +0100

    Todolist en classe

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -5,28 +5,58 @@ import PropTypes from 'prop-types';
 // == Import
 import './styles.css';
 
-// == Composant
-const Counter = ({ count }) => {
-  let message = '';
+// eslint-disable-next-line react/prefer-stateless-function
+class Counter extends React.Component {
+  // La méthode dédiée au rendu du composant
+  // === au composant sous forme de function
+  render() {
+    console.log('render : Counter');
+    const count = 123;
 
-  switch (count) {
-    case 0:
-      message = 'Aucune tâche en cours';
-      break;
-    case 1:
-      message = 'Une tâche en cours';
-      break;
-    default:
-      message = `${count} tâches en cours`;
-      break;
+    let message = '';
+
+    switch (count) {
+      case 0:
+        message = 'Aucune tâche en cours';
+        break;
+      case 1:
+        message = 'Une tâche en cours';
+        break;
+      default:
+        message = `${count} tâches en cours`;
+        break;
+    }
+
+    return (
+      <div id="todo-counter">
+        {message}
+      </div>
+    );
   }
+}
 
-  return (
-    <div id="todo-counter">
-      {message}
-    </div>
-  );
-};
+// == Composant
+// const Counter = ({ count }) => {
+//   let message = '';
+
+//   switch (count) {
+//     case 0:
+//       message = 'Aucune tâche en cours';
+//       break;
+//     case 1:
+//       message = 'Une tâche en cours';
+//       break;
+//     default:
+//       message = `${count} tâches en cours`;
+//       break;
+//   }
+
+//   return (
+//     <div id="todo-counter">
+//       {message}
+//     </div>
+//   );
+// };
 
 Counter.propTypes = {
   count: PropTypes.number.isRequired,
--- src/components/Todolist/index.js
+++ src/components/Todolist/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React, { useState } from 'react';
+import React from 'react';
 
 // == Import
 import './style.css';
@@ -10,23 +10,56 @@ import tasksData from 'src/data/tasks';
 // selectors
 import { getTasksUndone } from 'src/selectors/tasks';
 
+// console.log(new React.Component)
 // == Composant
-const Todolist = () => {
-  const tasks = tasksData;
-  // mise en place du state
-  // const [tasks, setTasks] = useState(tasksData);
+// eslint-disable-next-line react/prefer-stateless-function
+class Todolist extends React.Component {
+  // == __construct. est appelé lors de l'instanciation : <Todolist /> => new Todolist()
+  // surcharge du constructeur du parent
+  // on récupère les props
+  constructor(props) {
+    // Mais on veut que le constructeur du parent d'exécute quand même
+    // On appelle le constructeur du parent : les étapes de création obligatoires de React
+    // On fournit les props au constructeur du parent (qu'il les prépare)
+    super(props);
+    console.log('constructor : instanciation');
+    // Je définis le state du composant (sous forme de classe) dans son constructeur
+    // React reconnait la valeur `state` et lui applique un traitement spécifique
+    this.state = {
+      message: 'coucou',
+    };
+  }
 
-  // utilisation d'un selector pour obtenir les tâches non effectuées
-  const count = getTasksUndone(tasks).length;
+  // Rendu du composant (lié à react)
+  render() {
+    console.log('render : rendu du composant');
+    const tasks = tasksData;
+    // utilisation d'un selector pour obtenir les tâches non effectuées
+    const count = getTasksUndone(tasks).length;
 
-  return (
-    <div id="todo">
-      <Form />
-      <Counter count={count} />
-      <List tasks={tasks} />
-    </div>
-  );
-};
+    return (
+      <div id="todo">
+        <Form />
+        <Counter count={count} />
+        <List tasks={tasks} />
+      </div>
+    );
+  }
+}
+
+// const Todolist = () => {
+//   const tasks = tasksData;
+//   // utilisation d'un selector pour obtenir les tâches non effectuées
+//   const count = getTasksUndone(tasks).length;
+
+//   return (
+//     <div id="todo">
+//       <Form />
+//       <Counter count={count} />
+//       <List tasks={tasks} />
+//     </div>
+//   );
+// };
 
 // == Export
 export default Todolist;
